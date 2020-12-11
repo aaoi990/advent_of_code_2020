1320e91547ae6c0a722d2470d7c01332bc9f07f2
amanda <a.thomson404@gmail.com>
2020-12-11 19:31:44 +0000

    day 2 part 2

--- 2/main.py
+++ 2/main.py
@@ -5,6 +5,11 @@ def main() -> None:
     with open('input.txt') as f:
         lines = f.read().splitlines()
 
+    print(f'{part_one(lines)}')
+    print(f'{part_two(lines)}')
+
+
+def part_one(lines):
     suitable = 0
     for line in lines:
         extracted = line.replace('-', ' ').replace(':', '').split(' ')
@@ -12,7 +17,18 @@ def main() -> None:
         if letter_count >= int(extracted[0]) and letter_count <= int(extracted[1]):
             suitable += 1
 
-    print(f'{suitable}')
+    return suitable
+
+
+def part_two(lines):
+    suitable = 0
+    for line in lines:
+        extracted = line.replace('-', ' ').replace(':', '').split(' ')
+        if((extracted[3][int(extracted[0]) - 1] == extracted[2])
+                is not (extracted[3][int(extracted[1]) - 1] == extracted[2])):
+            suitable += 1
+
+    return suitable
 
 
 if __name__ == "__main__":
