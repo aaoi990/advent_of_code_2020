6100de9b3789f3fd1ed653400f36421da2c0c5d2
amanda <a.thomson404@gmail.com>
2020-12-13 14:28:32 +0000

    Day 5 part 2

--- 5/main.py
+++ 5/main.py
@@ -5,16 +5,26 @@ def main() -> None:
     with open('input.txt') as f:
         lines = f.read().splitlines()
 
-    print(f'{part_one(lines)}')
+    seat_numbers = part_one(lines)
+    print(f'{part_two(seat_numbers)}')
 
 
-def part_one(lines) -> int:
+def part_one(lines) -> list:
     seat_numbers = []
     for line in lines:
         seat_numbers.append(int(line.replace("F", "0").replace(
             "B", "1").replace("L", "0").replace("R", "1"), 2))
 
-    return (max(seat_numbers))
+    return seat_numbers
+
+
+def part_two(lines) -> int:
+    missing = 0
+    for line in lines:
+        if line + 1 not in lines:
+            missing = line
+
+    return missing + 1
 
 
 if __name__ == "__main__":
