b2a8c9d212dce0c017e376d64afe576564c3dd82
Amanda <a.thomson404@googlemail.com>
2021-01-05 16:06:58 +0000

    Ehhh messy 9 pt 2

--- 9/main.py
+++ 9/main.py
@@ -7,7 +7,10 @@ def main() -> None:
             int(line)
             for line in f.read().splitlines()
         ]
-    print(part_1(instructions))
+
+    solution_part_one = part_1(instructions)
+    print(solution_part_one)
+    part_2(solution_part_one, instructions)
 
 
 def part_1(instructions) -> int:
@@ -26,8 +29,24 @@ def isValid(instructions, index, preamble):
                 return True
 
 
-def part_2(numbers) -> int:
-    pass
+def part_2(solution_part_one, instructions) -> int:
+    startPoint = 0
+    while testTotal(solution_part_one, instructions, startPoint) == True:
+        startPoint += 1
+
+
+def testTotal(solution, instructions, startPoint):
+    total = 0
+    for i in range(startPoint, len(instructions)):
+        total = total + instructions[i]
+        if(total > solution):
+            break
+        elif(total == solution):
+            solution_arr = instructions[startPoint:i+1]
+            if(len(solution_arr) > 2):
+                print(max(solution_arr) + min(solution_arr))
+                return False
+    return True
 
 
 if __name__ == "__main__":
